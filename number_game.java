cc01cd961f9343aea537eef97a4dda0d69bb9cbb
ananyasingh906 <ananyasingh906@gmail.com>
2023-10-02 10:59:22 +0530

    Number game v2

--- number_game.java
+++ number_game.java
@@ -1,13 +1,17 @@
 import java.util.*;
 
 class number_game{
-  public static void main(String[] args){
-    int guess, r;
+  void game(int r){
+    int guess, count_tries = 0;
     Scanner sc = new Scanner(System.in);
-    r = (int) (Math.random() * (100 - 10)) + 10;
     do{
-      System.out.println("Guess the random number: ");
+      System.out.println("Guess the random number(You'll only get 3 tries): ");
       guess = sc.nextInt();
+      count_tries++;
+      if(count_tries == 3){
+        System.out.println("Better luck next time");
+        break;
+      }
       if(guess == r)
         System.out.println("Exact guess!");
       if(guess < r)
@@ -15,5 +19,17 @@ class number_game{
       if(guess > r)
         System.out.println("Too high");
     }while(guess != r);
+  }
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    number_game obj=new number_game();
+    int r = (int) (Math.random() * (100 - 10)) + 10;
+    int choice;
+    do{
+      obj.game(r);
+      System.out.println("Do you want to play again? [1/0]: ");
+      choice = sc.nextInt();
+    }while(choice != 0);
+    
   }
 }
